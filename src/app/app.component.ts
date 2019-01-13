6abf0da5839d0e854f7493775b35fa22667122cf
abby-shabi <abbyshabi@gmail.com>
2019-01-13 11:51:46 +0300

    imported Quote from quote.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  title = 'LittyQuotes';
+  quotes =[
+    new Quote (1,'Life','My life is my message',' Mahatma Gandhi',new Date(2018,3,14)),
+    new Quote (2,'Life','My life is my message',' Mahatma Gandhi',new Date(2018,6,9) ),
+    new Quote (3,'Life','My life is my message',' Mahatma Gandhi',new Date(2018,7,12)),
+    new Quote (4,'Life','My life is my message',' Mahatma Gandhi',new Date(2018,4,18)),
+    new Quote (5,'Life','My life is my message',' Mahatma Gandhi',new Date(2018,2,14)),
+    new Quote (6,'Life','My life is my message',' Mahatma Gandhi',new Date(2018,11,14)),
+    new Quote (7,'Life','My life is my message',' Mahatma Gandhi',new Date(2019,0,10)),
+  ]
+
+
+constructor(){
+
 }
+
+}
+
